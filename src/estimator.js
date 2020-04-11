1067d1bcbdaae0146b2a435dbcdf62f7d9847127
Samuel <mbuthia.jsamuel@gmail.com>
2020-04-11 19:27:57 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,34 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) =>{ 
+    data = {
+        region: {
+            name: "Africa",
+            avgAge: 19.7,
+            avgDailyIncomeInUSD: 5,
+            avgDailyIncomePopulation: 0.71
+          },
+        periodType: "days",
+        timeToElapse: 58,
+        reportedCases: 674,
+        population: 66622705,
+        totalHospitalBeds: 1380614
+      }
+    
+    return {
+         // Best case estimation
+        impact: {
+            currentlyInfected = data.reportedCases * 10,
+            infectionsByRequestedTime = currentlyInfected * 512,
+
+        },    
+         //   Severe Case Estimation
+        severeImpact: {
+            currentlyInfected = data.reportedCases * 50,
+            infectionsByRequestedTime = currentlyInfected * 512,
+        },  
+        
+    }
+
+};
+
 
 export default covid19ImpactEstimator;
