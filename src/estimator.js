71ff74f5f95327e153629f301aa6b0a2c4d9e75d
Samuel <mbuthia.jsamuel@gmail.com>
2020-04-11 20:36:44 +0300

    changed formating

--- src/estimator.js
+++ src/estimator.js
@@ -1,32 +1,32 @@
-const covid19ImpactEstimator = (data) =>{ 
-    data = {
-        region: {
-            name: "Africa",
-            avgAge: 19.7,
-            avgDailyIncomeInUSD: 5,
-            avgDailyIncomePopulation: 0.71
-          },
-        periodType: "days",
-        timeToElapse: 58,
-        reportedCases: 674,
-        population: 66622705,
-        totalHospitalBeds: 1380614
-      }
+const covid19ImpactEstimator = (data) => { 
+  data = {
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  }
     
-    return {
+  return {
          // Best case estimation
-        impact: {
-            currentlyInfected : data.reportedCases * 10,
-            infectionsByRequestedTime: currentlyInfected * 512,
+    impact: {
+        currentlyInfected : data.reportedCases * 10,
+        infectionsByRequestedTime: currentlyInfected * 512,
 
-        },    
+    },    
          //   Severe Case Estimation
-        severeImpact: {
-            currentlyInfected: data.reportedCases * 50,
-            infectionsByRequestedTime : currentlyInfected * 512,
-        },  
+    severeImpact: {
+        currentlyInfected: data.reportedCases * 50,
+        infectionsByRequestedTime : currentlyInfected * 512,
+    },  
         
-    }
+  }
 
 };
 
